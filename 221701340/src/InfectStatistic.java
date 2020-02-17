46bb7d1eca15e7548c564155cfcbd43e41b76d37
fzu-hhj <60642165+fzu-hhj@users.noreply.github.com>
2020-02-17 16:20:16 +0800

    feat Read file

--- 221701340/src/InfectStatistic.java
+++ 221701340/src/InfectStatistic.java
@@ -1,4 +1,7 @@
 package infectSta;
+
+import java.io.File;
+
 /**
  * InfectStatistic
  * TODO
@@ -7,22 +10,68 @@ package infectSta;
  * @version xxx
  * @since xxx
  */
+class Date{
+	private int year;
+	private int month;
+	private int day;
+	Date(String dateStr){
+		String date[] = dateStr.split("-");
+		try {
+			System.out.println("02");///////////////////////*
+			year = Integer.parseInt(date[0]);
+			month = Integer.parseInt(date[1]);
+			day = Integer.parseInt(date[2]);
+		}catch(NumberFormatException e) {
+			System.out.println(e);
+		}
+	}
+	Date(){
+		year = 2020;
+		month = 12;
+		day =31;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	public int getMonth(){
+		return month;
+	}
+	public int getDay() {
+		return day;
+	}
+	public boolean earlier(Date commandDate){
+		if(year > commandDate.getYear()) {
+			return false;
+		}
+		if(month > commandDate.getMonth()) {
+			return false;
+		}
+		if(day > commandDate.getDay()) {
+			return false;
+		}
+		return true;
+	}
+}
 class Command {
-	private String date = "latest";
+	private Date date = new Date();
+	private int  kinds = 4;//记录输出的人的种数
 	private boolean ip = true;
 	private boolean sp = true;
 	private boolean cure = true;
 	private boolean dead = true;
-	private String[] province;
+	private String[] province = {"全国","福建","湖北"};
 	private String logFile;
 	private String outFile;
+	
 	Command(String[] commandStr){
 		
 		if( ! (isCommand(commandStr) && parseCommand(commandStr))) {
 			System.out.println("命令格式错误！");
 		}	
 	}
-	public String getDate() {
+	
+	public Date getDate() {
 		return date;
 	}
 	public boolean getIp() {
@@ -45,6 +94,9 @@ class Command {
 	}
 	public String getOutFile() {
 		return outFile;
+	}
+	public int getKinds() {
+		return kinds;
 	}
 	
 	/*初步判断命令的格式是否正确*/
@@ -74,7 +126,7 @@ class Command {
 					if(!commandStr[i+1].matches(dateFormat)) {
 						return false;
 					}
-					this.date = commandStr[i+1];
+					this.date = new Date(commandStr[i+1]);
 				}
 				if(tempStr.equals("-province")) {
 					//检验省份的输入是否为中文
@@ -98,21 +150,30 @@ class Command {
 					this.outFile = commandStr[i+1];
 				}
 				if(tempStr.equals("-type")) {
+					this.ip = false;
+					this.sp = false;
+					this.cure = false;
+					this.dead = false;
+					this.kinds = 0;
 					for(int j = 0; j < 4;j ++) {
 						if(commandStr[i+j].matches(regex)) {
 							return false;
 						}
 						else if(commandStr[i+j].equals("ip")) {
 							this.ip = true;
+							this.kinds ++;
 						}
 						else if(commandStr[i+j].equals("sp")) {
 							this.sp = true;
+							this.kinds ++;
 						}
 						else if(commandStr[i+j].equals("cure")) {
 							this.cure = true;
+							this.kinds ++;
 						}
 						else if(commandStr[i+j].equals("dead")) {
 							this.dead = true;
+							this.kinds ++;
 						}else {
 							return false;
 						}
@@ -130,12 +191,47 @@ class Command {
 	}
 
 }
+
+class FileIO{
+	private Command command;
+	private int[][] num;//存储指定省的人数
+	FileIO(Command command){
+		this.command = command;
+		int m = command.getProvince().length;
+		int n = command.getKinds();
+		num = new int[m][n] ;
+	}
+	public void fileIn() {
+		System.out.println("01");
+		String encoding = "UTF-8";
+		String dir = command.getLogFile();
+		File[] files = new File(dir).listFiles();
+		for(File file : files) {
+			if(file.isFile() && file.exists()) {
+				String fileDateStr = file.getName().substring(0, 10);
+				System.out.println(fileDateStr);
+				Date fileDate =new Date(fileDateStr);
+				if(fileDate.earlier(command.getDate())) {
+					//System.out.println("判断日期成功！");
+					
+				}
+				//System.out.println("读入文件成功！");
+			}
+		}
+		
+	}
+	public void fileOut() {
+		
+	}
+}
 public class InfectStatistic {
 	
 
     public static void main(String[] args) {
     	Command command = new Command(args);
-		System.out.println(command.getDate());
+		FileIO fileIO = new FileIO(command);
+		fileIO.fileIn();
+		//fileIO.fileOut();
 		System.out.println("程序结束！");
         System.out.println("helloworld");
     }
